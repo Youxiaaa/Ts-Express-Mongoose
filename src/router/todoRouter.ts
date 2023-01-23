76020ccc5c33e748285ff576d1186aa785bc5d75
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2023-01-23 09:52:11 +0800

    update

--- src/router/todoRouter.ts
+++ src/router/todoRouter.ts
@@ -6,7 +6,7 @@ const router = Router()
 
 router.get('/get', todoController.get)
 router.post('/add', filterLanguege, todoController.add)
-router.put('/update/:id', filterLanguege, todoController.update)
+router.put('/update/:id', todoController.update)
 router.delete('/delete/:id', todoController.delete)
 
 export default router
