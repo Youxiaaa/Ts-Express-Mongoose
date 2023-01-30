96eb2d2addc1b3c31e4921eaa0a0ae3a277a7c07
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2023-01-31 00:29:25 +0800

    update

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -45,13 +45,14 @@ export default {
         code: 401,
         message: '帳號或密碼錯誤'
       })
-      // 設定 token 過期時間
-      const expiresIn = '1d'
-      const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn })
+      const accessToken = jwt.sign({ userId: user._id, type: 'accessToken' }, 'secret', { expiresIn: '10m' })
+      const refreshToken = jwt.sign({ userId: user._id, type: 'refreshToken' }, 'refreshSecret', { expiresIn: '1d' })
+
       return res.status(200).send({
         code: 200,
         message: '登入成功',
-        token
+        accessToken,
+        refreshToken
       })
     } catch (err) {
       return res.status(422).send({
@@ -59,5 +60,45 @@ export default {
         message: err
       })
     }
+  },
+  refreshToken: async (req: any, res: any) => {
+    const { refreshToken } = req.body
+
+    if (!refreshToken) {
+      return res.status(401).send({
+        code: 401,
+        message: '拒絕訪問'
+      })
+    } else {
+      const isValid = await verifyToken(refreshToken)
+      if (isValid) {
+        const refreshDecoded = await jwt.verify(refreshToken, 'refreshSecret')
+        const access_token = jwt.sign({ userId: refreshDecoded.userId, type: 'accessToken' }, 'secret', { expiresIn: '10m' })
+        const refresh_token = jwt.sign({ userId: refreshDecoded.userId, type: 'refreshToken' }, 'refreshSecret', { expiresIn: '1d' })
+
+        return res.status(200).send({
+          code: 200,
+          message: '刷新Token成功',
+          access_token,
+          refresh_token
+        })
+      } else {
+        return res.status(401).send({
+          code: 401,
+          message: '刷新Token失敗'
+        })
+      }
+    }
+  }
+}
+
+async function verifyToken (refreshToken: string) {
+  try {
+    const refreshDecoded = await jwt.verify(refreshToken, 'refreshSecret')
+  
+    if (refreshDecoded) return true
+    return false
+  } catch (err) {
+    return false
   }
 }
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -5,5 +5,6 @@ const router = Router()
 
 router.post('/register', controllers.userController.register)
 router.post('/login', controllers.userController.login)
+router.post('/refreshToken', controllers.userController.refreshToken)
 
 export default router
